93efae846defdc855bcc50127658cd9f17d722e6
Gifty Arthur <arthgi01@luther.edu>
2015-10-22 23:54:57 -0500

    Update todospa.js

--- todospa.js
+++ todospa.js
@@ -1,1 +1,19 @@
+<script type = "text/javascript">
 
+        newToDo = function () {
+        var list = document.querySelector("#hello");
+        
+        li = document.createElement("li");
+        var checkBox = document.createElement("input");
+        checkBox.type = "checkbox";
+        task = document.querySelector("#iChange").value;
+        toDo = document.createTextNode(task);
+        
+        li.appendChild(checkBox);
+        li.appendChild(toDo);
+        li.className = document.querySelector("#priority").value;
+        list.appendChild(li)
+
+}
+
+</script>
